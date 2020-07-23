01acbdb0dd7dc3eab6c05796107bd6bae61f603e
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-07-23 13:27:08 +0200

    profile model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=100, blank=True, default='')
+    name = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['created']
