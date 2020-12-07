dd86a462fa9801ce3140e6a9d8a7b969582984c8
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-12-07 11:10:42 +0100

    feat(metierApi): logic

---
+++ metierApi/logic.py
@@ -0,0 +1,20 @@
+// ALGORYTHME METIERS
+
+//Récup JSON 
+
+//Récup Metiers en base de donnees (entrer des metiers en db locale pour tester) -> tableau
+
+//Parcours du tableau avec JSON récupéré pour comparer ->
+
+//Recherche de matches avec 3 lettres 
+
+//Recherche de matches avec 2 lettres
+
+//Recherche de matches avec 1 lettres
+
+// Classer par ordre croissant les résultats trouvés par indice de priorité (indices de priorités : 1 si 3 matches, 2 si 2 matches, 3 si 1 matche)
+//(Eventuellement faire un random pour chaque catégorie de priorités dans le cas où il y a de très nombreux métiers pour éviter que ça soit les mêmes qui remontent à chaque fois ?)
+
+// Récupérer les 3 premiers
+
+//Renvoi de trois métiers en JSON
